8020fdc39da7099b2e08921a62e09667a041c051
prashant-yadav-dot <shanydv001@gmail.com>
2026-09-09 16:42:18 +0530

    Remove API route handling in worker.js
    Removed API route handling from fetch function.

--- worker.js
+++ worker.js
@@ -1,23 +1,5 @@
 export default {
   async fetch(request, env) {
-    const url = new URL(request.url);
-
-    // API routes ko yahan handle kar sakte ho
-    if (url.pathname.startsWith("/api/")) {
-      return new Response(
-        JSON.stringify({
-          success: true,
-          message: "MoveOn API is working"
-        }),
-        {
-          headers: {
-            "Content-Type": "application/json"
-          }
-        }
-      );
-    }
-
-    // Baaki website assets serve karo
     return env.ASSETS.fetch(request);
   }
 };
