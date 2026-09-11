7e8353faa5d0dddfe7bfc3f016c7be4e7e7da37c
prashant-yadav-dot <shanydv001@gmail.com>
2026-09-11 17:21:57 +0530

    Update app.js

--- Moveon/app.js
+++ Moveon/app.js
@@ -393,7 +393,7 @@ export function redirectIfLoggedIn() {
                 );
 
                 window.location.replace(
-                    "home.html"
+                    "index.html"
                 );
 
             }
